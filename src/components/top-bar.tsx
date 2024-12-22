be485993a759155692614daa884e3af6a18ad35e
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-12-22 17:49:14 +0200

    sidebar done

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Bell, Search, Menu } from 'lucide-react';
+import { Bell, Search, User, Menu } from 'lucide-react';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
@@ -16,27 +16,21 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export function TopBar() {
   return (
-    <div className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+    <div className="sticky top-0 z-50 w-[80rem] border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-10">
+      <div className="flex items-center justify-between h-16">
         <div className="mr-4 hidden md:flex">
           <SidebarTrigger />
         </div>
-        <div className="mr-4 flex md:hidden">
-          <Button variant="ghost" size="icon">
-            <Menu className="h-5 w-5" />
-            <span className="sr-only">Toggle menu</span>
-          </Button>
+        <div className="w-full flex-1 md:w-auto md:flex-none">
+          <form className="relative">
+            <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              placeholder="Search inventory..."
+              className="pl-8 md:w-[300px] lg:w-[400px]"
+            />
+          </form>
         </div>
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-          <div className="w-full flex-1 md:w-auto md:flex-none">
-            <form className="relative">
-              <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input
-                placeholder="Search inventory..."
-                className="pl-8 md:w-[300px] lg:w-[400px]"
-              />
-            </form>
-          </div>
+        <div>
           <div className="flex items-center space-x-2">
             <Button variant="ghost" size="icon" className="md:hidden">
               <Search className="h-5 w-5" />
